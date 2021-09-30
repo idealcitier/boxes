4360d6e33d7194300eb0c3eb121fb567f9cd8f8c
idealcitier <liguo7@foxmail.com>
2021-09-30 20:04:55 +0800

    modify Node from struct to class

--- tree/tree.cpp
+++ tree/tree.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
 
-struct Node {
-    Node *left;
-    Node *right;
-    int value;
+class Node {
+public:
+	Node(int value) {
+		this->value = value;
+	}
+
+	void setLeftNode(Node *node) {
+		left = node;
+	}
+
+	void setRightNode(Node *node) {
+		right = node;
+	}
+
+private:
+	Node *left;
+	Node *right;
+	int value;
 };
 
 int main() {
-    
+    Node *n1 = new Node(1);
+    Node *n2 = new Node(1);
+    Node *n3 = new Node(1);
+    Node *n4 = new Node(1);
 }
